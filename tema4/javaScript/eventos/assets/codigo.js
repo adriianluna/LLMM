204bb9d69bdf09fdc21f41ba2438357fdbcd7a53
adriianluna <alundom2203@g.educaand.es>
2025-02-05 13:45:43 +0100

    asdffff

--- tema4/javaScript/eventos/assets/codigo.js
+++ tema4/javaScript/eventos/assets/codigo.js
@@ -3,7 +3,7 @@ const b = document.querySelector("body");
 const p2 = document.querySelector("#nidea2");
 
 p1.addEventListener("click",() => alert("Parrafo 1 click"));
-b.addEventListener("load",mensaje);
+b.addEventListener("load",(mensaje));
 p2.addEventListener("dblclick",() => alert("Parrafo 2 click"));
 
 function mensaje() {
