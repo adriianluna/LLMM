d5fc3cc7d50a31f10dfbe78f21ddce9c181ddbd8
adriianluna <alundom2203@g.educaand.es>
2025-02-10 09:26:24 +0100

    eventos

--- tema4/javaScript/eventos/ejercicio3/Js/ejercicio3.js
+++ tema4/javaScript/eventos/ejercicio3/Js/ejercicio3.js
@@ -2,16 +2,71 @@ const p1 = document.querySelector(".dblclick");
 const b = document.querySelector(".alert");
 const b2 = document.querySelector("body");
 const p2 = document.querySelector(".cursor");
-const botom = document.querySelector(".buttom");
+const boton = document.querySelector(".button");
+const robot = document.querySelector(".robot");
+const ninja = document.querySelector(".tortuga");
+const cursor = document.querySelector(".cursor");
 
 p1.addEventListener("dblclick",() => alert("Parrafo 2 click"));
 b.addEventListener("click",(mensaje));
-b2.addEventListener("load",mensaje());
+b2.addEventListener("load",mensaje2());
 p2.addEventListener("dblclick",() => alert("Parrafo 2 click"));
-botom.addEventListener("botom",(pulsar));
+boton.addEventListener("click",(pulsar));
+robot.addEventListener("click",(numeroMayorMenor));
+// tortuga ninja
+ninja.addEventListener("mouseleave",(caclulaParImpar));
+cursor.addEventListener("mouseenter",(calculaMultiplicacion));
+
+function calculaMultiplicacion(){
+
+    let num1 = parseInt(prompt("Introduce un número: "));
+    let num2 = parseInt(prompt("Introduce un número: "));
+    if( isNaN(num1) &&  isNaN(num2)){
+        alert("Los numeros no son validos");
+    }else{
+        let multiplicacion = num1 * num2;
+        alert("La multiplicacion de  " + num1  + " y " + + num2 + " es " + multiplicacion);
+    }
+
+}
+
+
+function numeroMayorMenor(){
+ 
+    let num1 = parseInt(prompt("Introduce un número: "));
+    let num2 = parseInt(prompt("Introduce un número: "));
+    if( isNaN(num1) &&  isNaN(num2)){
+        alert("los numeros no son validos");
+        }else{
+            if(num1 > num2){
+                alert("El " + num1 + " es mayor");
+            }else if(num1 == num2){
+                alert("Los numeros son iguales");
+            }else{
+                alert("El " + num2 + " es mayor");
+            }
+       } 
+       
+    
+
+}
+
+function caclulaParImpar(){
+    let num = parseInt(prompt("Introduce un número: "));
+    if(num % 2 == 0){
+        alert("El numero es par");
+    }else{
+        alert("El numero es impar");
+    }
+}
+
 
 function mensaje() {
-    alert("Pagina cargada");
+    alert("Has hecho un click");
+}
+
+function mensaje2() {
+    alert("Bienvenidos");
 }
 
 function pulsar() {
