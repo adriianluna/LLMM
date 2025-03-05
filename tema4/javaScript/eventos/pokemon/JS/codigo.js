570e8df079049a2a54d5285511c0a71415d56dd2
adriianluna <alundom2203@g.educaand.es>
2025-03-05 13:03:13 +0100

    pokemon

---
+++ tema4/javaScript/eventos/pokemon/JS/codigo.js
@@ -0,0 +1,41 @@
+
+// constantes
+const nombrePokemon = document.querySelector(".nombre");
+const imagenPokemon = document.querySelector(".imagen");
+const alturaPokemon = document.querySelector(".altura");
+const tipoPokemon = document.querySelector(".tipo");
+
+
+
+// Constante del boton
+const boton = document.querySelector(".boton");
+boton.addEventListener("click", bucarPokemon);
+// Funcion
+function bucarPokemon() {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
