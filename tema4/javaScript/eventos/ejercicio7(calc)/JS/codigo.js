f2939f173126cd76807b9cf38784e6386c38ffc0
adriianluna <alundom2203@g.educaand.es>
2025-03-13 12:50:12 +0100

    calc

---
+++ tema4/javaScript/eventos/ejercicio7(calc)/JS/codigo.js
@@ -0,0 +1,39 @@
+
+
+
+
+// COntantes para la suma y eso
+const funciones = document.querySelectorAll(".funcion")
+
+// Boton para borrar el interior del input
+const borrar = document.querySelector(".borrar");
+
+// Boton para ver el resultado
+const resultado = document.querySelector(".resultado");
+
+// Constantes  ppara los numeros 
+const botones = document.querySelectorAll(".boton");
+
+let op = "";
+
+botones.addEventListener("click", numeros);
+
+botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+
+        //if(op = )
+
+
+
+    }
+)
+
+
+
+}
+
+)
+
+
+
+
